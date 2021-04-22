55c263404f1dafbbed1beda968730c1d0770dbe4
zendraft <zendraft.pro@gmail.com>
2021-04-22 16:12:12 +0200

    Set chat format

---
+++ src/main/java/ga/ecloriamc/listener/PlayerChatEvent.java
@@ -0,0 +1,20 @@
+package ga.ecloriamc.listener;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.UserManager;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerChatEvent implements Listener {
+    @EventHandler
+    public void onPlayerChat(org.bukkit.event.player.PlayerChatEvent e){
+        LuckPerms lP = LuckPermsProvider.get();
+        UserManager uM = lP.getUserManager();
+        Player p = e.getPlayer();
+        String prefix = uM.getUser(e.getPlayer().getName()).getCachedData().getMetaData().getPrefix();
+        e.setFormat(ChatColor.translateAlternateColorCodes('&', prefix +"&f"+p.getName()+" &e&l> &7"+e.getMessage() ));
+    }
+}
